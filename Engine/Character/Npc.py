69febc402a0491275f252cd36d0b5dbb272c1291
Skanky0815 <rico-schulz@web.de>
2020-12-18 00:50:34 +0100

    some refactoring

--- Engine/Character/Npc.py
+++ Engine/Character/Npc.py
@@ -1,71 +1,50 @@
 import json
-from typing import List
+from typing import List, Dict
 
-from Engine.Character.Character import BaseCharacter
-from Engine.Weapon import NpcWeapon
+from Engine.Character.Behavior import Behavior
+from Engine.Character.Character import Character
 
 
-class Npc(BaseCharacter):
+class Npc(Character):
 
     def __init__(
             self,
             name: str,
             race: str,
-            attention: int,
-            attention_modifier: int,
-            charisma: int,
-            charisma_modifier: int,
-            agility: int,
-            agility_modifier: int,
-            secrecy: int,
-            secrecy_modifier: int,
-            precision: int,
-            precision_modifier: int,
-            acumen: int,
-            acumen_modifier: int,
-            strength: int,
-            strength_modifier: int,
-            willpower: int,
-            willpower_modifier: int,
-            corruption: int,
-            defense: int,
+            health: int,
+            dodge: int,
             armor: int,
-            weapons: List[any]
+            actions: int,
+            behaviors: Dict
     ):
-        super().__init__(name, race, attention, charisma, agility, secrecy, precision, acumen, strength, willpower)
-        self.corruption: int = corruption
-        self.attention_modifier: int = attention_modifier
-        self.charisma_modifier: int = charisma_modifier
-        self.agility_modifier: int = agility_modifier
-        self.secrecy_modifier: int = secrecy_modifier
-        self.precision_modifier: int = precision_modifier
-        self.acumen_modifier: int = acumen_modifier
-        self.strength_modifier: int = strength_modifier
-        self.willpower_modifier: int = willpower_modifier
-        self.defense: int = defense
-        self.armor: int = armor
-        self.weapons: List[NpcWeapon] = []
+        super(Npc, self).__init__(name=name, health=health, armor=armor)
+        self.actions: int = actions
+        self.race: str = race
+        self.dodge: int = dodge
+        self.behaviors: List[Behavior] = []
 
-        for weapon in weapons:
-            self.weapons.append(NpcWeapon(**weapon))
+        for behavior in behaviors:
+            print(behavior)
+            self.behaviors.append(Behavior(**behavior))
 
     def __str__(self) -> str:
-        weapons: str = ""
-        for weapon in self.weapons:
-            weapons += f"\tName: {weapon.name}, Schaden {weapon.damage}\n"
+        behaviors: str = ""
+        for behavior in self.behaviors:
+            behaviors += f"\t{behavior}\n"
 
         return "\n" \
             f"Name: {self.name}\n" \
             f"Race: {self.race}\n" \
-            f"Zähigkeit: {self.toughness}/{self.perma_toughness}\tSchmerzgrenze: {self.pain_barrier}\n" \
+            f"Leben: {self.health}/{self.max_health}\n" \
             "---------------------------------------\n" \
-            f"Aufmerksamkeit:\t{self.attention} ({self.attention_modifier})\tAusstrahlung:\t{self.charisma} ({self.charisma_modifier})\n" \
-            f"Gewandheit:\t\t{self.agility} ({self.agility_modifier})\tHeimlichkeit:\t{self.secrecy} ({self.secrecy_modifier})\n" \
-            f"Präzision:\t\t{self.precision} ({self.precision_modifier})\tScharfsinn:\t\t{self.acumen} ({self.acumen_modifier})\n" \
-            f"Stärke:\t\t\t{self.strength} ({self.strength_modifier})\tWillenskraft:\t{self.willpower} ({self.willpower_modifier})\n" \
-            "---------------------------------------\n" \
-            f"Waffen:\n{weapons}" \
+            f"Verhalten:\n{behaviors}" \
             "---------------------------------------\n"
+
+    def is_alive(self):
+        return self.health > 0
+
+    def get_behavior(self, worth_comparing: int) -> Behavior:
+        return next(behavior for behavior in self.behaviors if behavior.in_range(worth_comparing))
 
 
 class Factory:
